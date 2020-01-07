99f0990945bdfb69c110db9a1a0da0f9c47af206
Francesco Biscani <bluescarni@gmail.com>
2020-01-07 14:30:25 +0100

    Try only 1 engine.

--- tools/circleci_bionic_gcc7_pagmo_head.sh
+++ tools/circleci_bionic_gcc7_pagmo_head.sh
@@ -41,7 +41,7 @@ make -j2 install VERBOSE=1
 cd
 
 # Start ipcluster.
-ipcluster  start --debug --n=4 &
+ipcluster  start --debug --n=1 &
 sleep 20
 
 python -c "import pygmo; pygmo.test.run_test_suite(0)"
