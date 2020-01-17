8ccb996bbb20b4722cd3cd9913eee4e937b5917d
Francesco Biscani <bluescarni@gmail.com>
2020-01-17 16:29:28 +0100

    From latest to head for the docs.

--- tools/circleci_bionic_gcc7_pagmo_latest_37.sh
+++ tools/circleci_bionic_gcc7_pagmo_latest_37.sh
@@ -15,7 +15,7 @@ export deps_dir=$HOME/local
 export PATH="$HOME/miniconda/bin:$PATH"
 bash miniconda.sh -b -p $HOME/miniconda
 conda config --add channels conda-forge --force
-conda_pkgs="cmake boost-cpp python=3.7 numpy cloudpickle dill numba pip pybind11 pagmo-devel sphinx nbsphinx sphinx_rtd_theme"
+conda_pkgs="cmake boost-cpp python=3.7 numpy cloudpickle dill numba pip pybind11 pagmo-devel"
 conda create -q -p $deps_dir -y
 source activate $deps_dir
 conda install $conda_pkgs -y
@@ -32,53 +32,5 @@ cd
 # Run the test suite.
 python -c "import pygmo; pygmo.test.run_test_suite(1); pygmo.mp_island.shutdown_pool(); pygmo.mp_bfe.shutdown_pool()"
 
-# Build the documentation.
-cd ~/project/doc
-export SPHINX_OUTPUT=`make html linkcheck 2>&1 |grep -v Warning  > /dev/null`;
-
-if [[ "${SPHINX_OUTPUT}" != "" ]]; then
-    echo "Sphinx encountered some problem:";
-    echo "${SPHINX_OUTPUT}";
-    exit 1;
-fi
-echo "Sphinx ran successfully";
-
-if [[ ! -z "${CI_PULL_REQUEST}" ]]; then
-    echo "Testing a pull request, the generated documentation will not be uploaded.";
-    exit 0;
-fi
-
-if [[ "${CIRCLE_BRANCH}" != "master" ]]; then
-    echo "Branch is not master, the generated documentation will not be uploaded.";
-    exit 0;
-fi
-
-# Check out the gh_pages branch in a separate dir.
-cd ../..
-git config --global push.default simple
-git config --global user.name "CircleCI"
-git config --global user.email "bluescarni@gmail.com"
-set +x
-git clone "https://${GH_ACCESS_TOKEN}@github.com/esa/pygmo2.git" pygmo2_gh_pages -q
-set -x
-cd pygmo2_gh_pages
-git checkout -b gh-pages --track origin/gh-pages;
-git rm -fr *;
-mv ../project/doc/_build/html/* .;
-git add *;
-# We assume here that a failure in commit means that there's nothing
-# to commit.
-git commit -m "Update Sphinx documentation, commit ${CIRCLE_SHA1} [skip ci]." || exit 0
-PUSH_COUNTER=0
-until git push -q
-do
-    git pull -q
-    PUSH_COUNTER=$((PUSH_COUNTER + 1))
-    if [ "$PUSH_COUNTER" -gt 3 ]; then
-        echo "Push failed, aborting.";
-        exit 1;
-    fi
-done
- 
 set +e
 set +x
