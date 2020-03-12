d0853fd16f8284c0838df08b8ecd1905b47104a3
Moritz v. Looz <Moritz.v.Looz@esa.int>
2020-03-12 17:54:38 +0100

    added docstrings and moved helper class into wrapper class

--- pygmo/_py_algorithms.py
+++ pygmo/_py_algorithms.py
@@ -1,3 +1,11 @@
+# Copyright 2020 PaGMO development team
+#
+# This file is part of the pygmo library.
+#
+# This Source Code Form is subject to the terms of the Mozilla
+# Public License v. 2.0. If a copy of the MPL was not distributed
+# with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 import random
 import warnings
 
@@ -8,19 +16,60 @@ from scipy.optimize import NonlinearConstraint, minimize
 def _generate_gradient_sparsity_wrapper(
     func, idx, shape, sparsity_func, invert_sign=False
 ):
+    """
+    A function to extract a sparse gradient from a pygmo problem to a dense gradient expectecd by scipy.
 
+    Pygmo convention is to include problem constraints into its fitness function. The same applies to the gradient.
+    The scipy.optimize.minimize function expects a separate callable for each constraint, this function creates a wrapper that extracts a requested dimension.
+    It also transforms the sparse gradient into a dense representation.
+
+    Args:
+
+        func: the gradient callable
+        idx: the requested dimension.
+        shape: the shape of the result as interpreted by numpy. Should be (dim) for a problem of input dimension dim.
+        sparsity_func: a callable giving the sparsity pattern. Use problem.gradient_sparsity.
+        invert_sign: whether all values of the gradient should be multiplied with -1. This is necessary for inequality constraints, where the feasible side is interpreted the opposite way by scipy and pygmo.
+
+    Returns:
+
+        a callable that passes all arguments to the gradient callable func and returns the dense gradient at dimension idx
+
+    Raises:
+
+        unspecified: any exception thrown by sparsity_func
+
+
+    """
     sparsity = sparsity_func()
     sign = 1
     if invert_sign:
         sign = -1
 
     def wrapper(*args, **kwargs):
-        # Here we get back just one one-dimensional gradient, including all dimensions and constraints
+        """
+        Calls the gradient callable and returns dense representation along a fixed dimension
+        
+        Args:
+
+            args: arguments for callable
+            kwargs: keyword arguments for callable
+
+        Returns:
+
+            dense representation of gradient
+
+        Raises:
+
+            ValueError: If number of non-zeros in gradient and sparsity pattern disagree
+            unspecified: any exception thrown by wrapped callable
+
+        """
         sparse_values = func(*args, **kwargs)
         nnz = len(sparse_values)
         if nnz != len(sparsity):
             raise ValueError(
-                "Sparse gradient/hessian has "
+                "Sparse gradient has "
                 + str(nnz)
                 + " non-zeros, but sparsity pattern has "
                 + str(len(sparsity))
@@ -40,18 +89,60 @@ def _generate_gradient_sparsity_wrapper(
 def _generate_hessian_sparsity_wrapper(
     func, idx, shape, sparsity_func, invert_sign=False
 ):
+    """
+    A function to extract a hessian gradient from a pygmo problem to a dense hessian expectecd by scipy.
 
+    Pygmo convention is to include problem constraints into its fitness function. The same applies to the hessian
+    The scipy.optimize.minimize function expects separate callables for the fitness function and each constraint.
+    This function creates a wrapper that extracts a requested dimension and also transforms the sparse hessian into a dense representation.
+
+    Keyword args:
+
+        func: the hessian callable
+        idx: the requested dimension.
+        shape: the shape of the result as interpreted by numpy. Should be (dim,dim) for a problem of input dimension dim.
+        sparsity_func: a callable giving the sparsity pattern. Use problem.hessians_sparsity.
+        invert_sign: whether all values of the hessian should be multiplied with -1. This is necessary for inequality constraints, where the feasible side is interpreted the opposite way by scipy and pygmo.
+
+    Returns:
+
+        a callable that passes all arguments to the hessian callable func and returns the dense hessian at dimension idx
+
+    Raises:
+
+        unspecified: any exception thrown by sparsity_func
+
+
+    """
     sparsity = sparsity_func()[idx]
     sign = 1
     if invert_sign:
         sign = -1
 
     def wrapper(*args, **kwargs):
+        """
+        Calls the hessian callable and returns dense representation along a fixed dimension
+        
+        Args:
+
+            args: arguments for callable
+            kwargs: keyword arguments for callable
+
+        Returns:
+
+            dense representation of hessian
+
+        Raises:
+
+            ValueError: If number of non-zeros in hessian and sparsity pattern disagree
+            unspecified: any exception thrown by wrapped callable
+
+        """
         sparse_values = func(*args, **kwargs)[idx]
         nnz = len(sparse_values)
         if nnz != len(sparsity):
             raise ValueError(
-                "Sparse gradient/hessian has "
+                "Sparse hessian has "
                 + str(nnz)
                 + " non-zeros, but sparsity pattern has "
                 + str(len(sparsity))
@@ -66,57 +157,77 @@ def _generate_hessian_sparsity_wrapper(
     return wrapper
 
 
-class _fitness_cache:
-    def __init__(self, problem):
-        self.problem = problem
-        self.args = None
-        self.kwargs = None
-        self.result = None
-
-    def update_cache(self, *args, **kwargs):
-        if True or not (self.args == args and self.kwargs == kwargs):
-            # print("Updating fitness")
-            self.args = args
-            self.kwargs = kwargs
-            self.result = self.problem.fitness(*args, **kwargs)
-        else:
-            # print("Keeping cached fitness")
-            pass
-
-    def fitness(self, *args, **kwargs):
-        self.update_cache(*args, **kwargs)
-        return self.result[: self.problem.get_nobj()]
-
-    def generate_eq_constraint(self, i):
-        def eqFunc(*args, **kwargs):
-            self.update_cache(*args, **kwargs)
-            return self.result[self.problem.get_nobj() + i]
-
-        return eqFunc
-
-    def generate_neq_constraint(self, i):
-        def neqFunc(*args, **kwargs):
-            self.update_cache(*args, **kwargs)
-            # In pagmo, inequality constraints have to be negative, in scipy they have to be non-negative.
-            return -self.result[self.problem.get_nobj() + self.problem.get_nec() + i]
-
-        return neqFunc
-
-
 class scipy:
     """
     This class is a user defined algorithm (UDA) providing a wrapper around the function scipy.optimize.minimize.
-    The constructor accepts those arguments that are specific to the algorithm:
-    - args
-    - method
-    - tol - the tolerance
-    - callback
-    - options
 
-    The problem bounds and the existence of a gradient and hessian are deduced calling the relevant problem methods (problem.has_gradient(), etc..)
+    Construction arguments are those options of scipy.optimize.minimize that are not problem-specific. 
+    The problem-specific ones, for example the bounds, constraints and the existence of a gradient and hessian, are deduced when calling evolve.
     """
 
-    def __init__(self, args=(), method=None, tol=None, callback=None, options=None):
+    class _fitness_cache:
+        """
+        Cache to avoid multiple evaluations of the fitness functions for the same parameters.
+        This is necessary since pygmo.problem evaluates all constraints with the fitness function,
+        but scipy expects a different callable for each constraint.
+        """
+
+        def __init__(self, problem):
+            self.problem = problem
+            self.args = None
+            self.kwargs = None
+            self.result = None
+
+        def update_cache_if_necessary(self, *args, **kwargs):
+            if True or not (self.args == args and self.kwargs == kwargs):  # TODO: fix!
+                # Updating fitness
+                self.args = args
+                self.kwargs = kwargs
+                self.result = self.problem.fitness(*args, **kwargs)
+
+        def fitness(self, *args, **kwargs):
+            self.update_cache_if_necessary(*args, **kwargs)
+            return self.result[: self.problem.get_nobj()]
+
+        def generate_eq_constraint(self, i):
+            def eqFunc(*args, **kwargs):
+                self.update_cache_if_necessary(*args, **kwargs)
+                return self.result[self.problem.get_nobj() + i]
+
+            return eqFunc
+
+        def generate_neq_constraint(self, i):
+            def neqFunc(*args, **kwargs):
+                self.update_cache_if_necessary(*args, **kwargs)
+                # In pagmo, inequality constraints have to be negative, in scipy they have to be non-negative.
+                return -self.result[
+                    self.problem.get_nobj() + self.problem.get_nec() + i
+                ]
+
+            return neqFunc
+
+    def __init__(
+        self,
+        args=(),
+        method: str = None,
+        tol: float = None,
+        callback=None,
+        options: dict = None,
+    ) -> None:
+        """
+            Args:
+
+                args: optional - extra arguments for fitness callable
+                method: optional - string specifying the method to be used by scipy. From scipy docs: "If not given, chosen to be one of BFGS, L-BFGS-B, SLSQP, depending if the problem has constraints or bounds."
+                tol: optional - tolerance for termination
+                callback: optional - callable that is called in each iteration, independent from the fitness function
+                options: optional - dict of solver-specific options
+
+            Raises:
+
+                ValueError: If method is not one of Nelder-Mead Powell, CG, BFGS, Newton-CG, L-BFGS-B, TNC, COBYLA, SLSQP, trust-constr, dogleg, trust-ncg, trust-exact, trust-krylov or None.
+
+        """
         method_list = [
             "Nelder-Mead",
             "Powell",
@@ -136,7 +247,12 @@ class scipy:
         if method in method_list + [None]:
             self.method = method
         else:
-            raise ValueError("Method not supported: " + method)
+            raise ValueError(
+                "Method "
+                + str(method)
+                + " not supported, only the following "
+                + str(method_list)
+            )
 
         self.args = args
         self.tol = tol
@@ -145,10 +261,24 @@ class scipy:
 
     def evolve(self, population):
         """
-        Take a random member of the population, use it as initial guess
-        for calling scipy.optimize.minimize and replace it with the final result.
+        Call scipy.optimize.minimize with a random member of the population as start value.
 
-        Modifies the given population and returns it.
+        The problem is extracted from the population and its fitness function gives the objective value for the optimization process.
+
+        Args:
+
+            population: The population containing the problem and a set of initial solutions
+
+        Returns:
+
+            The changed population
+
+        Raises:
+
+            ValueError: If the problem has constraints, but during construction a method was selected that cannot deal with them.
+            ValueError: If the problem contains multiple objectives
+            ValueError: If the problem is stochastic
+            unspecified: any exception thrown the member functions of the problem
         """
         problem = population.problem
 
@@ -198,7 +328,7 @@ class scipy:
         idx = random.randint(0, len(population) - 1)
         if problem.get_nc() > 0:
             # Need to handle constraints, put them in a wrapper to avoid multiple fitness evaluations.
-            fitness_wrapper = _fitness_cache(problem)
+            fitness_wrapper = scipy._fitness_cache(problem)
             constraints = []
             if self.method in ["COBYLA", "SLSQP", None]:
                 for i in range(problem.get_nec()):
@@ -331,7 +461,16 @@ class scipy:
 
     def set_verbosity(self, level: int) -> None:
         """
-        Modifies the 'disp' parameter in the options dict. Every verbosity level above zero sets it to true.
+        Modifies the 'disp' parameter in the options dict, which prints out a final convergence message.
+
+        Args:
+
+            level: Every verbosity level above zero prints out a convergence message.
+
+        Raises:
+
+            ValueError: If options dict was given in instance constructor and has conflicting options
+
         """
         if level > 0:
             if self.options is None:
