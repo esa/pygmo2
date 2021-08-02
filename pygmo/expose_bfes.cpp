7173ce291679019e86b4727c13b16fda70410228
Francesco Biscani <bluescarni@gmail.com>
2021-08-02 16:46:47 +0200

    Update copyright dates.

--- pygmo/expose_bfes.cpp
+++ pygmo/expose_bfes.cpp
@@ -1,4 +1,4 @@
-// Copyright 2020 PaGMO development team
+// Copyright 2020, 2021 PaGMO development team
 //
 // This file is part of the pygmo library.
 //
--- pygmo/plotting/__init__.py
+++ pygmo/plotting/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright 2017-2018 PaGMO development team
+# Copyright 2017, 2018, 2019, 2020, 2021 PaGMO development team
 #
 # This file is part of the PaGMO library.
 #
