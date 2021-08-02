b336cc19b583692547f8d7b71312616a55bdcf2b
Francesco Biscani <bluescarni@gmail.com>
2021-08-02 10:39:18 +0200

    Script fix.

--- tools/gha_windows-2016.ps1
+++ tools/gha_windows-2016.ps1
@@ -33,7 +33,7 @@ cmake `
     -DCMAKE_PREFIX_PATH=C:\Miniconda\envs\pygmo `
     -DCMAKE_INSTALL_PREFIX=C:\Miniconda\envs\pygmo `
     -DBoost_NO_BOOST_CMAKE=ON `
-    -DPYGMO_ENABLE_IPO=yes ^
+    -DPYGMO_ENABLE_IPO=yes `
     ..
 
 cmake --build . --config Release --target install
