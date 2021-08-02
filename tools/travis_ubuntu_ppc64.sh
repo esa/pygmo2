f8675425c689b396333af69581d53b240adbe734
Francesco Biscani <bluescarni@gmail.com>
2021-08-02 23:01:03 +0200

    Script tweaks.

--- tools/travis_ubuntu_ppc64.sh
+++ tools/travis_ubuntu_ppc64.sh
@@ -22,7 +22,7 @@ git clone https://github.com/esa/pagmo2.git
 cd pagmo2
 mkdir build
 cd build
-cmake ../ -DCMAKE_BUILD_TYPE=Debug -DBoost_NO_BOOST_CMAKE=ON -DPAGMO_WITH_EIGEN3=ON -DPAGMO_WITH_NLOPT=ON -DCMAKE_PREFIX_PATH=$deps_dir -DCMAKE_INSTALL_PREFIX=$deps_dir -DPAGMO_ENABLE_IPO=ON
+cmake ../ -DCMAKE_BUILD_TYPE=Debug -DBoost_NO_BOOST_CMAKE=ON -DPAGMO_WITH_EIGEN3=ON -DPAGMO_WITH_NLOPT=ON -DCMAKE_PREFIX_PATH=$deps_dir -DCMAKE_INSTALL_PREFIX=$deps_dir -DPAGMO_ENABLE_IPO=ON -DPAGMO_INSTALL_LIBDIR=lib
 make -j4 install VERBOSE=1
 cd ..
 cd ..
