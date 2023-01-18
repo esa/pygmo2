7d0bfc3a389291bb9dd67fd047e5383eabb5e114
Dario Izzo <dario.izzo@gmail.com>
2023-01-18 15:45:05 +0100

    told ya! this is the last one .. reordering and reactivating stuff

--- tools/circleci_bionic_conda_pagmo_head_310.sh
+++ tools/circleci_bionic_conda_pagmo_head_310.sh
@@ -45,49 +45,5 @@ python -c "import pygmo; pygmo.test.run_test_suite(1); pygmo.mp_island.shutdown_
 cd ~/project/tools
 python circleci_additional_tests.py
 
-# Build the documentation.
-cd ~/project/doc
-make html linkcheck
-
-# Run the doctests.
-make doctest;
-
-if [[ ! -z "${CI_PULL_REQUEST}" ]]; then
-    echo "Testing a pull request, the generated documentation will not be uploaded.";
-    exit 0;
-fi
-
-if [[ "${CIRCLE_BRANCH}" != "master" ]]; then
-    echo "Branch is not master, the generated documentation will not be uploaded.";
-    exit 0;
-fi
-
-# Check out the gh_pages branch in a separate dir.
-cd ../..
-git config --global push.default simple
-git config --global user.name "CircleCI"
-git config --global user.email "bluescarni@gmail.com"
-set +x
-git clone "https://${GH_ACCESS_TOKEN}@github.com/esa/pygmo2.git" pygmo2_gh_pages -q
-set -x
-cd pygmo2_gh_pages
-git checkout -b gh-pages --track origin/gh-pages;
-git rm -fr *;
-mv ../project/doc/_build/html/* .;
-git add *;
-# We assume here that a failure in commit means that there's nothing
-# to commit.
-git commit -m "Update Sphinx documentation, commit ${CIRCLE_SHA1} [skip ci]." || exit 0
-PUSH_COUNTER=0
-until git push -q
-do
-    git pull -q
-    PUSH_COUNTER=$((PUSH_COUNTER + 1))
-    if [ "$PUSH_COUNTER" -gt 3 ]; then
-        echo "Push failed, aborting.";
-        exit 1;
-    fi
-done
-
 set +e
 set +x
